2375303a4d5215a6bb4b30f01e10a107d48711b8
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-04 23:56:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,8 +4,7 @@
 
 
 def fizzbuzz(int)
-  int = gets().to_i
-  puts(int)
+
 
   if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
   puts "FizzBuzz"
@@ -17,3 +16,5 @@ def fizzbuzz(int)
     puts "nil"
   end
 end
+
+fizzbuzz(int)
