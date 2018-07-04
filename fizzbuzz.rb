0427a2b75953aa1d09897576a8ef829b990a2195
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-04 22:30:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,15 +4,14 @@
 
 
 def fizzbuzz(int)
-  int = gets.strip
+  int = gets.to_i
 
-
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 && int % 5 == 0# if the number int is divisible by 3
+  puts "FizzBuzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
     puts "Fizz" # Go fizz
   elsif int % 5 == 0
     puts "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0# if the number int is divisible by 3
-    puts "FizzBuzz"
   else
     puts "nil"
   end
