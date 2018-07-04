e1b6cf78dba490e58fe88e768a6c16d3232398ed
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-04 23:39:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,6 @@
 
 def fizzbuzz(int)
 
-for int in 1..100
   if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
   puts "FizzBuzz"
   elsif int % 3 == 0 # if the number int is divisible by 3
