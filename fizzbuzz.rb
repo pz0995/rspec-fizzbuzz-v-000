3043774fd5764eb0c39f70f1a2114a7219bb01d2
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-04 23:47:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,7 +6,7 @@
 def fizzbuzz(int)
   range = 1..100
   range.each do |int|
-    
+
   if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
   puts "FizzBuzz"
   elsif int % 3 == 0 # if the number int is divisible by 3
