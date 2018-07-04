1977ce53036e9ffee488b07f6f49402d649719a1
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-04 22:17:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-int = gets.to_i
+
 
 def fizzbuzz(int)
+  int = gets.to_i
+  
   int = 1
   if int % 3 == 0 # if the number int is divisible by 3
     puts "Fizz" # Go fizz
