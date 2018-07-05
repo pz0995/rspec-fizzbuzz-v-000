0e1b4132fb68cd6b4e78a2ec3da94a12c146134e
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-05 00:19:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-
-
 def fizzbuzz(int)
-  int = gets.to_i
-  puts "fizzbuzz(#{int})"
+
+
 
 
   if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
