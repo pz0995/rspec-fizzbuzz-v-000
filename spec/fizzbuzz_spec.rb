de4d06ec0d79444c0e2c079b21bb6b68e883b587
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-05 00:25:48 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,17 +4,17 @@ require_relative '../fizzbuzz.rb'
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
-    fizz_3 = fizzbuzz(3)
+    fizzbuzz(3) == "Fizz"
 
     expect(fizz_3).to eq("Fizz")
   end
   it 'returns "Buzz" when the number is divisible by 5' do
-    fizz_5 = fizzbuzz(5)
+    fizzbuzz(5) == "Buzz"
 
     expect(fizz_5).to eq("Buzz")
   end
   it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
-    fizz_15 = fizzbuzz(15)
+    fizzbuzz(15)  == "FizzBuzz"
 
     expect(fizz_15).to eq("FizzBuzz")
   end
